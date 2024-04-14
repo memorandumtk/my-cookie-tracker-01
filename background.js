84da3b45aab04474283f4a1572bff0653746fcf7
memorandumtk <memorandum876@outlook.com>
2024-04-14 16:43:25 -0700

    Just Created table.

--- background.js
+++ background.js
@@ -1,40 +1,39 @@
 console.log('Background script loaded');
+
 // Storage initialization (if it doesn't exist)
 chrome.storage.local.get("cookieData", (data) => {
-    if (!data.cookieData) {
-        console.log('Initializing cookieData')
-        chrome.storage.local.set({ cookieData: {} });
-    }
+  if (!data.cookieData) {
+    console.log('Initializing cookieData')
+    chrome.storage.local.set({ cookieData: {} });
+  }
 });
 
 // Listener for cookie changes
 chrome.cookies.onChanged.addListener((changeInfo) => {
-    if (!changeInfo.removed) { // Focus on new or modified cookies
-        console.log(changeInfo.cookie);
-        const site = changeInfo.cookie.domain;
-        const name = changeInfo.cookie.name;
-        const value = changeInfo.cookie.value;
-        const expirationDate = changeInfo.cookie.expirationDate;
+  if (!changeInfo.removed) { 
+    console.log(changeInfo.cookie);
 
-        chrome.storage.local.get("cookieData", (data) => {
-            let cookieData = data.cookieData || {};
-            if (!cookieData[site]) {
-                cookieData[site] = {};
-            } else if (cookieData[site]) {
-                cookieData[site][name] = { value, expirationDate }
-                chrome.storage.local.set({ cookieData });
-            } else if (cookieData[site][name]) {
-                storedData = { value, expirationDate }
-                cookieData[site][name] = storedData;
-                chrome.storage.local.set({ cookieData });
-                console.log(storedData);
-            }
-            console.log('Cookie data updated');
-        });
-    }
+    const site = changeInfo.cookie.domain;
+    const name = changeInfo.cookie.name;
+    const value = changeInfo.cookie.value;
+    const expirationDate = changeInfo.cookie.expirationDate;
+    const storedData = { value, expirationDate };
+
+    chrome.storage.local.get("cookieData", (data) => {
+      let cookieData = data.cookieData || {};
+
+      if (!cookieData[site]) {
+        cookieData[site] = {};
+      }
+
+      cookieData[site][name] = storedData; 
+      chrome.storage.local.set({ cookieData }); 
+      console.log('Cookie data updated');
+    });
+  }
 });
 
-
+// Example of `changeInfo.cookie`:
 // {domain: 'developer.chrome.com', expirationDate: 1746824218, hostOnly: true, httpOnly: false, name: 'cookies_accepted', …}
 // domain
 // :
