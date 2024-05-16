f769222b6ea6c20b0c48fae6f1e4f5b71ba87260
memorandumtk <memorandum876@outlook.com>
2024-05-16 10:14:02 -0400

    fix: Implemented stopPropagation at 'Remove' button not to trigger the event listener on a row. add: Sumarized what this extension does so far into README.md,

--- README.md
+++ README.md
@@ -1,7 +1,34 @@
 # Cookie Manager
-This is a chrome extension to manage cookie data of yours. When this chrome is enabled, the domain, the name and the information of the site that you added to your cookie.
+This is a chrome extension to manage cookie data of yours. When this chrome is enabled, the domain, the name and other information of the site that you added to your cookie.
 
+## How it works
+- After it is enabled, the `background.js` listens to the cookie change event using chrome cookie API.
+- When the cookie change event is triggered, the cookie data is stored in the `chrome.storage.local` as `cookieData`. Each data will be stored as `storedData` into `cookieData`.
+- The `popup.js` will get the `cookieData` from the `chrome.storage.local` and display the data in the `popup.html` using table.
+- The `popup.html` will display the cookie data in the table with domain, name, expiration date and `Remove` button. If `Remove` button is clicked, the cookie data will be removed from the `chrome.storage.local` and the table will be updated with removing the row.
+- If you click the table row, you can see the detail of the cookie data in the `popup.html`. It is done by the logic below.
+    1. When the table row is clicked, the `cookieData` is retrieved from the `chrome.storage.local`.
+    2. The `cookieData` is picked up by the domain and the name of the cookie data.
+    3. The picked data is passed to the `backgrouond.js` using `chrome.runtime.sendMessage`
+    4. the `background.js` will store the detail of the cookie data to the `chrome.runtime.storage` with key: `details`. If it's stored successfully, the `backgound.js` will open the `details.html` as optionsPage.
+    5. The `details.html` will be open and display the detail of the cookie data.
+   > The reason to pass though `background.js` once is two reasons. One is that it is not possible to pass the data directly from `popup.js` to `details.html`. The other is that the `background.js` can store the data sent from `popup.html` so that I can ensure that the cookie data selected can be used in `details.html`.
+
+---
+
+## How to use
+1. Import and enable the extension.
+2. Cookie data after the extension is enabled, it will automatically be recorded through chrome cookie API.
+3. Clicking the icon, you can see the cookie information you have in `popup.html`.
+4. In the `popup.html`, a table will be displayed with each of the cookie data like domain, name and expiration date and `Remove` button, which will delete the cookie data.
+5. If you would like to see detail of a cookie data, you can see it clicking the table row of the cookie.
+
+
+---
+
+### Example of cookie data object
 background.js Line 22, storedData example.
+```text
 {
     "domain": ".google.com",
     "expirationDate": 1747262997.74793,
@@ -15,3 +42,4 @@ background.js Line 22, storedData example.
     "storeId": "0",
     "value": "AKEyXzWttUc7n5cyktxQIDpxvn1wwirId2_f-tii5JL_5mFpMQFqpOsRlA9yrrO9sZJ2E1Z5l0A"
 }
+```
--- utils/popup/tableGenerate.js
+++ utils/popup/tableGenerate.js
@@ -47,10 +47,12 @@ const removeCookie = async (domain, name, e) => {
             delete cookieData[domain];
         }
 
+        // Then, set the updated cookieData to storage.
         chrome.storage.local.set({ cookieData });
         console.log('after');
         console.log(data);
     });
+    // Remove the cookie from the browser too.
     chrome.cookies.remove({ url: `https://${domain}`, name: name });
     // Remove the row from the table.
     e.target.closest('tr').remove();
@@ -75,6 +77,7 @@ function rowGenerate (index, domain, storedData) {
         value = 'No value.';
         expirationDate = 'No expiration date.';
     }
+
     // domain td
     let domainTd = document.createElement('td');
     let domainAnchor = document.createElement('a');
@@ -84,39 +87,45 @@ function rowGenerate (index, domain, storedData) {
     domainAnchor.href = `https://${modifiedDomain}`;
     domainAnchor.target = '_blank';
     domainTd.appendChild(domainAnchor);
+
     // name td
     let nameTd = document.createElement('td');
     nameTd.textContent = name;
+
     // value td
     let valueTd = document.createElement('td');
     valueTd.textContent = value;
     valueTd.classList.add('value-cell');
+
     // expiration td
     let expirationTd = document.createElement('td');
     expirationTd.textContent = formattedExpirationDate;
+
     // remove button td
     let removeButtonTd = document.createElement('td');
     let removeButton = document.createElement('button');
     removeButton.textContent = 'Remove';
-    removeButton.addEventListener('click', (e) => {
-        removeCookie(domain, name, e);
+    removeButton.addEventListener('click', async (e) => {
+        e.stopPropagation(); // Prevent the click event from propagating to the row
+        await removeCookie(domain, name, e);
     });
     removeButtonTd.appendChild(removeButton);
-    // append all tds to row
+
+    // Append each child elements to `row`.
     let row = document.createElement('tr');
     row.id = `row-${index}`;
-
-    row.addEventListener('click', async (e) => {
-        console.log('row clicked')
-        let detailsToBeSent = {domain: domain, name: name, details: storedData, event: e};
-        await messageSend(detailsToBeSent)
-    });
-
     row.appendChild(domainTd);
     row.appendChild(nameTd);
     row.appendChild(valueTd);
     row.appendChild(expirationTd);
     row.appendChild(removeButtonTd);
+
+    // Add event listener to the row.
+    row.addEventListener('click', async (e) => {
+        console.log('row clicked')
+        let detailsToBeSent = {domain: domain, name: name, details: storedData, event: e};
+        await messageSend(detailsToBeSent)
+    });
 
     return row;
 }
@@ -130,6 +139,8 @@ function tableGenerate (data) {
     let table = document.createElement('table');
     let header = headerGenerate(data);
     table.appendChild(header);
+    // Generate rows for each domain. `data` is an object: `data.cookieData`
+    // which is with domain as key and storedData as value.
     for (const [index, [domain, storedData]] of Object.entries(data).entries()) {
         let row = rowGenerate(index, domain, storedData);
         table.appendChild(row);
